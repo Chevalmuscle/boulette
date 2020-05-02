4fadbfcc56495875eeff14f327754317c20c2c50
chevalmuscle <me@marcantoinejean.com>
2020-05-01 22:53:25 -0400

    users can join games & ready up

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,73 @@
+module.exports = { playerListUpdate, turnUpdate };
+
 const express = require("express");
+const socket = require("socket.io");
+const Game = require("./game");
+
 const app = express();
 const port = process.env.PORT || 3001;
+
+const games = {};
+
+/**
+ * Delay before an empty game gets deleted (in ms)
+ */
+const GAME_DELETION_DELAY = 5 * 60 * 1000;
 
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-app.listen(port, function () {
+const server = app.listen(port, function () {
   console.log(`http://localhost:${port} 🐷`);
 });
+
+const io = socket(server);
+
+io.on("connection", (socket) => {
+  console.log("New connection " + socket.id);
+  let room;
+
+  socket.on("join-room", ({ roomid, playerName }) => {
+    room = roomid;
+    roomid = roomid.toLowerCase();
+    if (games[roomid] === undefined) {
+      // io.to(socket.id).emit("invalid-room-id", null);
+      games[roomid] = new Game(roomid, 30000); //! 30000 is for dev purposes
+
+      socket.join(roomid);
+      games[roomid].addPlayer(socket.id, playerName);
+    } else {
+      socket.join(roomid);
+      games[roomid].addPlayer(socket.id, playerName);
+    }
+    room = roomid;
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`${socket.id} disconnected`);
+    if (room !== undefined) {
+      games[room].removePlayer(socket.id);
+
+      if (games[room].isEmpty()) {
+        // adds a delay before deleting the game
+        setTimeout(function () {
+          if (games[room].isEmpty()) {
+            delete games[room];
+            console.log("Purged game with room " + room);
+          }
+        }, GAME_DELETION_DELAY);
+      }
+    }
+  });
+
+  socket.on("player-ready-state-update", (isReady) => {
+    games[room].setPlayerReadyState(socket.id, isReady);
+  });
+});
+
+function playerListUpdate(roomid, playerList) {
+  io.in(roomid).emit("player-list", playerList);
+}
+
+function turnUpdate(roomid, currentPlayerid, word, turnLength) {}
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,53 @@
-import React from "react";
+import React, { Component } from "react";
+import socketIOClient from "socket.io-client";
 
-function App() {
-  return <div>Hello world !</div>;
+const SOCKETIO_ENDPOINT = process.env.REACT_APP_BACKEND_URL;
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      socket: undefined,
+      isReady: false,
+    };
+
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.toggleReadyState = this.toggleReadyState.bind(this);
+  }
+
+  componentDidMount() {
+    const socket = socketIOClient(SOCKETIO_ENDPOINT);
+
+    socket.emit("join-room", { roomid: "1", playerName: "bob" });
+
+    socket.on("player-list", (playerList) => {
+      console.table(playerList);
+    });
+
+    socket.on("new-turn", ({ currentPlayerid, word }) => {
+      console.log(currentPlayerid + " " + word);
+    });
+
+    socket.on("counter", ({ timeLeft, totalTime }) => {
+      console.log(timeLeft + " / " + totalTime);
+    });
+
+    this.setState({ socket });
+  }
+
+  toggleReadyState() {
+    this.state.socket.emit("player-ready-state-update", !this.state.isReady);
+    this.setState({ isReady: !this.state.isReady });
+  }
+
+  render() {
+    return (
+      <div>
+        Hello world !
+        <button onClick={this.toggleReadyState}>
+          {this.state.isReady ? "Unready" : "Ready"}
+        </button>
+      </div>
+    );
+  }
 }
-
-export default App;
