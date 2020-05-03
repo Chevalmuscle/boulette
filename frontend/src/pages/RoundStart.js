c7b546bbf9c370d393cc7e8ed0a88f894557b135
chevalmuscle <me@marcantoinejean.com>
2020-05-02 20:27:35 -0400

    fixed RoundStart height

--- frontend/src/pages/RoundStart.js
+++ frontend/src/pages/RoundStart.js
@@ -1,31 +1,22 @@
 import React, { Component } from "react";
 
-import ProgressBar from "../components/ProgressBar";
-
 import styles from "./RoundStart.module.css";
 
 export default class RoundStart extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      percentage: 0,
-    };
-  }
-
   render() {
     return (
       <div className={styles["game-page"]}>
-        <h1>Ronde 1: Explications</h1>
-        <div className={styles["play-zone"]}>
-          <p>Jean-Marles</p>
+        <div className={`${this.props.end && styles["hide"]} ${styles["play-zone"]}`}>
+          <div className={styles["game-text"]}>{this.props.explanationText}</div>
+          <div className={styles["push"]}></div>
         </div>
-        <div className={styles["start-button-wrapper"]}>
-          <button className={styles["start-button"]}>Commencer</button>
-        </div>
-        <div className={styles["progress-bar"]}>
-          <ProgressBar percentage={this.state.percentage} />
-        </div>
+        <section class={styles["footer"]}>
+          <div className={styles["start-button-wrapper"]}>
+            <button onClick={this.props.handleGuessed} id={styles["start-button"]} className={styles["button"]}>
+              Commencer
+            </button>
+          </div>
+        </section>
       </div>
     );
   }
