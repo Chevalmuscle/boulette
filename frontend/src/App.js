c59f7c026404899877c2443c1a22adc8534f4634
chevalmuscle <me@marcantoinejean.com>
2020-05-02 21:50:36 -0400

    added UI to the game flow

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 import socketIOClient from "socket.io-client";
 
+import Play from "./pages/Play";
+import Spectate from "./pages/Spectate";
+import RoundStart from "./pages/RoundStart";
+import ProgressBar from "./components/ProgressBar";
+
+import styles from "./App.module.css";
+
 const SOCKETIO_ENDPOINT = process.env.REACT_APP_BACKEND_URL;
 
 export default class App extends Component {
@@ -8,23 +15,35 @@ export default class App extends Component {
     super(props);
     this.state = {
       socket: undefined,
+      playerid: undefined,
       isReady: false,
       word: undefined,
+      totalTime: undefined,
       timeLeft: undefined,
       timeIsOver: false,
+
+      gameState: "round",
     };
 
     this.componentDidMount = this.componentDidMount.bind(this);
     this.toggleReadyState = this.toggleReadyState.bind(this);
-    this.endTurn = this.endTurn.bind(this);
-    this.guessedWord = this.guessedWord.bind(this);
-    this.guessWordTurnEnd = this.guessWordTurnEnd.bind(this);
+    this.handleRoundStart = this.handleRoundStart.bind(this);
+    this.handleGuessed = this.handleGuessed.bind(this);
+    this.handleFailed = this.handleFailed.bind(this);
+    this.gameStart = this.gameStart.bind(this);
   }
 
   componentDidMount() {
-    const socket = socketIOClient(SOCKETIO_ENDPOINT);
+    const roomid = new URLSearchParams(this.props.location.search).get("room");
 
-    socket.emit("join-room", { roomid: "1", playerName: "bob" });
+    if (roomid === null) {
+      this.props.history.push("/");
+      return;
+    }
+    const socket = socketIOClient(SOCKETIO_ENDPOINT);
+    socket.on("connect", () => this.setState({ playerid: socket.id }));
+
+    socket.emit("join-room", { roomid: roomid, playerName: "bob" });
 
     socket.on("player-list", (playerList) => {
       console.table(playerList);
@@ -32,12 +51,13 @@ export default class App extends Component {
 
     socket.on("new-round", (roundIndex) => {
       console.log("new round " + roundIndex);
-      this.setState({ isReady: false });
+      this.setState({ gameState: "round", isReady: false });
     });
 
     socket.on("new-turn", (currentPlayerid) => {
       console.log(currentPlayerid);
       this.setState({ timeIsOver: false });
+      this.gameStart(currentPlayerid);
     });
 
     socket.on("new-word", (word) => {
@@ -47,7 +67,7 @@ export default class App extends Component {
 
     socket.on("counter", ({ timeLeft, totalTime }) => {
       console.log(timeLeft + " / " + totalTime);
-      this.setState({ timeLeft: timeLeft });
+      this.setState({ timeLeft: timeLeft, totalTime: totalTime });
     });
 
     socket.on("times-up", () => {
@@ -63,38 +83,82 @@ export default class App extends Component {
     this.setState({ isReady: !this.state.isReady });
   }
 
-  endTurn() {
+  gameStart(currentPlayerid) {
+    if (currentPlayerid === this.state.playerid) {
+      // the player is playing
+      this.setState({ timeIsOver: false, gameState: "playing" });
+    } else {
+      // the player is spectating
+      this.setState({ gameState: "spectating" });
+    }
+  }
+
+  handleRoundStart(e) {
+    e.preventDefault();
     this.state.socket.emit("turn-end");
   }
 
-  guessedWord() {
-    this.state.socket.emit("guessed-word", this.state.word);
+  handleGuessed(e) {
+    e.preventDefault();
+    if (this.state.timeIsOver) {
+      this.state.socket.emit("guessed-word-turn-end", {
+        word: this.state.word,
+        hasGuessed: true,
+      });
+      this.state.socket.emit("turn-end");
+    } else {
+      this.state.socket.emit("guessed-word", this.state.word);
+    }
     this.setState({ word: undefined });
   }
 
-  guessWordTurnEnd(hasGuessed) {
+  handleFailed(e) {
+    e.preventDefault();
     this.state.socket.emit("guessed-word-turn-end", {
       word: this.state.word,
-      hasGuessed: hasGuessed,
+      hasGuessed: false,
     });
     this.state.socket.emit("turn-end");
   }
 
   render() {
+    const gameState = (() => {
+      switch (this.state.gameState) {
+        case "round":
+          return (
+            <RoundStart
+              handleStart={this.handleRoundStart}
+              explanationText={"Vous devez expliquer le mot qui sera affiché"}
+            />
+          );
+        case "playing":
+          return (
+            <Play
+              title={"Ronde 1: Explications"}
+              playZoneText={this.state.word}
+              handleGuessed={this.handleGuessed}
+              handleFailed={this.handleFailed}
+              end={this.state.timeIsOver}
+            />
+          );
+        case "spectating":
+          return <Spectate playZoneText={"(Tour de Bob Gratton)"} />;
+        default:
+          return <div>{`game state: ${gameState}`}</div>;
+      }
+    })();
     return (
-      <div>
-        Hello world !
-        <button onClick={this.toggleReadyState}>
-          {this.state.isReady ? "Unready" : "Ready"}
-        </button>
-        <button onClick={this.endTurn}>End Turn</button>
-        <button onClick={this.guessedWord}>Guessed word</button>
-        {this.state.timeIsOver && (
-          <div>
-            <button onClick={() => this.guessWordTurnEnd(false)}>fail</button>
-            <button onClick={() => this.guessWordTurnEnd(true)}>success</button>
+      <div className={styles["container"]}>
+        <h1>Ronde 1: Explications</h1>
+        <div className={styles["content-container"]}>
+          <div className={styles["my_content"]}>{gameState}</div>
+          <div className={styles["push"]}></div>
+        </div>
+        <div className={styles["footer"]}>
+          <div className={styles["progress-bar"]}>
+            <ProgressBar percentage={(this.state.timeLeft / this.state.totalTime) * 100} />
           </div>
-        )}
+        </div>
       </div>
     );
   }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,18 +4,12 @@ import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import App from "./App";
-import Components from "./Components";
-import ChooseWords from "./pages/ChooseWords";
-import RoundStart from "./pages/RoundStart";
 
 import "./index.css";
 
 const routing = (
   <Router>
-      <Route exact path="/" component={App} />
-      <Route exact path="/components" component={Components} />
-      <Route exact path="/choose-words" component={ChooseWords} />
-      <Route exact path="/play" component={RoundStart} />
+      <Route exact path="/game" component={App} />
 
   </Router>
 );
